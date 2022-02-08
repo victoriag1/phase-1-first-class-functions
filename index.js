0b1442d0e3cb706e66ddbc88116e0d1984c9645c
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-02-07 19:46:08 -0800

    commit tree 13ac0a339322fca018bbbd78af4ede80ad556021

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(spy) {
+    console.log(spy())
+}
+receivesAFunction(spy)
+
+function returnsANamedFunction(name) {
+    console.log(name())
+ }
+ returnsANamedFunction (newName)
+  
+ function returnsAnAnonymousFunction() {
+  
+ }
+ 
